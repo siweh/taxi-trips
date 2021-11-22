69c0b0eb41f18d111f99cd77608730b61283d34c
siweh <sanemadesi@gmail.com>
2021-11-22 16:31:37 +0200

    Fix some test

--- sql/data.sql
+++ sql/data.sql
@@ -6,25 +6,25 @@ insert into routes(route_name, fare) values('N1', 35);
 insert into routes(route_name, fare) values('N12', 45);
 insert into routes(route_name, fare) values('R56', 15);
 
-insert into taxi(reg_number, region_id) values ('12', 1);
-insert into taxi(reg_number, region_id) values ('12', 2);
-insert into taxi(reg_number, region_id) values ('21', 3);
+-- insert into taxi(reg_number, region_id) values ('12', 1);
+-- insert into taxi(reg_number, region_id) values ('12', 2);
+-- insert into taxi(reg_number, region_id) values ('21', 3);
 
-delete from taxi;
+-- delete from taxi;
 
-insert into taxi(reg_number, region_id) values ('DB12345', 1);
-insert into taxi(reg_number, region_id) values ('CA19785', 2);
-insert into taxi(reg_number, region_id) values ('GP32456', 3);
+-- insert into taxi(reg_number, region_id) values ('DB12345', 1);
+-- insert into taxi(reg_number, region_id) values ('CA19785', 2);
+-- insert into taxi(reg_number, region_id) values ('GP32456', 3);
 
 
 
-insert into trips(taxi_id, route_id) values(4, 1);
-insert into trips(taxi_id, route_id) values(5, 1);
-insert into trips(taxi_id, route_id) values(5, 2);
-insert into trips(taxi_id, route_id) values(4, 2);
-insert into trips(taxi_id, route_id) values(6, 3);
-insert into trips(taxi_id, route_id) values(6, 3);
-insert into trips(taxi_id, route_id) values(5, 1);
+-- insert into trips(taxi_id, route_id) values(4, 1);
+-- insert into trips(taxi_id, route_id) values(5, 1);
+-- insert into trips(taxi_id, route_id) values(5, 2);
+-- insert into trips(taxi_id, route_id) values(4, 2);
+-- insert into trips(taxi_id, route_id) values(6, 3);
+-- insert into trips(taxi_id, route_id) values(6, 3);
+-- insert into trips(taxi_id, route_id) values(5, 1);
 
 -- select * from taxi
 -- join region on taxi.region_id = region.id
