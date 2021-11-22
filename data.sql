f4c4af153d3b3e0024de7409fa5d14f9c47cf975
siweh <sanemadesi@gmail.com>
2021-11-22 17:30:04 +0200

    Fixed all failing tests

--- data.sql
+++ data.sql
@@ -1,30 +1,30 @@
-insert into region(id, region_name) values (1, 'Durban');
-insert into region(id, region_name) values (2, 'Cape Town');
-insert into region(id, region_name) values (3, 'Gauteng');
+-- insert into region(id, region_name) values (1, 'Durban');
+-- insert into region(id, region_name) values (2, 'Cape Town');
+-- insert into region(id, region_name) values (3, 'Gauteng');
 
-insert into routes(route_name, fare) values('N1', 35);
-insert into routes(route_name, fare) values('N12', 45);
-insert into routes(route_name, fare) values('R56', 15);
+-- insert into routes(route_name, fare) values('N1', 35);
+-- insert into routes(route_name, fare) values('N12', 45);
+-- insert into routes(route_name, fare) values('R56', 15);
 
-insert into taxi(reg_number, region_id) values ('12', 1);
-insert into taxi(reg_number, region_id) values ('12', 2);
-insert into taxi(reg_number, region_id) values ('21', 3);
+-- insert into taxi(reg_number, region_id) values ('12', 1);
+-- insert into taxi(reg_number, region_id) values ('12', 2);
+-- insert into taxi(reg_number, region_id) values ('21', 3);
 
-delete from taxi;
+-- delete from taxi;
 
-insert into taxi(reg_number, region_id) values ('DB12345', 1);
-insert into taxi(reg_number, region_id) values ('CA19785', 2);
-insert into taxi(reg_number, region_id) values ('GP32456', 3);
+-- insert into taxi(reg_number, region_id) values ('DB12345', 1);
+-- insert into taxi(reg_number, region_id) values ('CA19785', 2);
+-- insert into taxi(reg_number, region_id) values ('GP32456', 3);
 
 
 
-insert into trips(taxi_id, route_id) values(4, 1);
-insert into trips(taxi_id, route_id) values(5, 1);
-insert into trips(taxi_id, route_id) values(5, 2);
-insert into trips(taxi_id, route_id) values(4, 2);
-insert into trips(taxi_id, route_id) values(6, 3);
-insert into trips(taxi_id, route_id) values(6, 3);
-insert into trips(taxi_id, route_id) values(5, 1);
+-- insert into trips(taxi_id, route_id) values(4, 1);
+-- insert into trips(taxi_id, route_id) values(5, 1);
+-- insert into trips(taxi_id, route_id) values(5, 2);
+-- insert into trips(taxi_id, route_id) values(4, 2);
+-- insert into trips(taxi_id, route_id) values(6, 3);
+-- insert into trips(taxi_id, route_id) values(6, 3);
+-- insert into trips(taxi_id, route_id) values(5, 1);
 
 -- select * from taxi
 -- join region on taxi.region_id = region.id
@@ -36,9 +36,17 @@ insert into trips(taxi_id, route_id) values(5, 1);
 
 -- select * from trips
 
-
-
 -- select * from trips
 -- join taxi on trips.taxi_id = taxi.id
 -- join region on taxi.region_id = region.id
 -- where region.region_name = 'Cape Town';
+
+-- select sum(fare) from routes
+-- join trips on trips.route_id = routes.id
+-- join taxi on taxi.id = trips.taxi_id
+-- where taxi.reg_number = 'DB12345';
+
+-- select sum(fare) as income from routes
+-- join trips on trips.route_id = routes.id
+-- join taxi on taxi.id = trips.taxi_id
+-- group by taxi.reg_number
